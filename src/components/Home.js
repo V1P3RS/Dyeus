db4eff5c132e57602e7174f7782ff5901b51be68
V1P3RS <86308839+V1P3RS@users.noreply.github.com>
2023-04-13 05:28:55 +0530

    change2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Login from "./components/Login/Login";
 
 function Home(props) {
   return (
     <div>
-      <div>
-        <h1>
-          <Link to="/login">Login</Link>
-        </h1>
-        <br />
-        <h1>
-          <Link to="/signup">Signup</Link>
-        </h1>
-      </div>
-
-      <br />
-      <br />
-      <br />
-      <h2>{props.name ? `Welcome - ${props.name}` : "Login Please"}</h2>
+      <Login />
     </div>
   );
 }
