e06eb37e2d58654ef2f9c8b83988796407b7ad8d
V1P3RS <86308839+V1P3RS@users.noreply.github.com>
2023-04-13 05:30:44 +0530

    Reverting the changes2

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,20 @@ import Login from "./components/Login/Login";
 function Home(props) {
   return (
     <div>
-      <Login />
+      <div>
+        <h1>
+          <Link to="/login">Login</Link>
+        </h1>
+        <br />
+        <h1>
+          <Link to="/signup">Signup</Link>
+        </h1>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <h2>{props.name ? `Welcome - ${props.name}` : "Login Please"}</h2>
     </div>
   );
 }
